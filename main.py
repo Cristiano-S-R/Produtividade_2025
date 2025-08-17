82946740d1533ff440a0ae59fa0e6ff21789803b
Cristiano-S-R <164667977+Cristiano-S-R@users.noreply.github.com>
2025-08-16 22:47:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import geopandas as gpd
 st.set_page_config(layout="wide")
 
 # Carregamento dados geoespacial do brasil
-local = 'https://drive.google.com/drive/folders/1pKRb41q2wQuIyBJRHH33dQezGXYSbKms?usp=sharing'
+local = 'https://drive.google.com/file/d/1LLsanCjUXpeGhujc9hZE40NOzMCkFxby/view?usp=drive_link'
 gdf = gpd.read_file(local)
 
 st.sidebar.image("logo_agro.jpg", use_container_width=True)
@@ -52,6 +52,7 @@ folium.GeoJson(
 
 # Folium_static com largura em pixels (ex.: 1200)
 folium_static(m, width=1200, height=520)
+
 
 
 
